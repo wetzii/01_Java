005e2650ffc87ad2b763e9c531d9299e41968221
wetzi <wetzi@192.168.8.33>
2026-01-11 11:57:47 +0100

    Snack finish

--- inventar/src/inventar/Snack.java
+++ inventar/src/inventar/Snack.java
@@ -1,13 +1,38 @@
 package inventar;
+public class Snack implements Sellable {
 
-public class Snack implements Sellable{
-	private String label;
-	private double priceCents;
-	private int stock;
-	
-	@Override
-	public static boolean isAvalibal{
-		System.out.println("fff");
-	}
+    private String label;
+    private double priceCents;
+    private int stock;
+
+    public Snack(String label, double priceCents, int stock) {
+        this.label = label;
+        this.priceCents = priceCents;
+        this.stock = stock;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public double getPriceCent() {
+        return priceCents;
+    }
+
+    @Override
+    public boolean isAvalibale() {
+        return stock > 0;
+    }
+
+    @Override
+    public void sellOne() {
+        if (isAvalibale()) {
+            stock--;
+            System.out.println(label + " verkauft. Restbestand: " + stock);
+        } else {
+            System.out.println(label + " nicht mehr verfügbar!");
+        }
+    }
 }
-
