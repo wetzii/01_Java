697fd78f37a8f4a85fc0e8564a606cd6958a58ab
wetzii <240062@o365.htl-leoben.at>
2026-01-11 13:59:06 +0100

    Souvenier

---
+++ inventar/src/inventar/Souvenir.java
@@ -0,0 +1,40 @@
+package inventar;
+
+public class Souvenir implements Sellable {
+
+    private String label;
+    private double priceCents;
+    private int stock;
+
+    public Souvenir(String label, double priceCents, int stock) {
+        this.label = label;
+        this.priceCents = priceCents;
+        this.stock = stock;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public double getPriceCent() {
+        return priceCents;
+    }
+
+    @Override
+    public boolean isAvalibale() {
+        return stock > 0;
+    }
+
+    @Override
+    public void sellOne() {
+    	
+        if (isAvalibale()) {
+            stock--;
+            System.out.println(label + " verkauft. Restbestand: " + stock);
+        } else {
+            System.out.println(label + " nicht mehr verfügbar!");
+        }
+    }
+}
