1661a1f1799f1c13cb23782379d4697693bf3a4e
wetzi <wetzi@Samsung-Degl-ARCH>
2026-01-14 10:59:44 +0100

    l

--- Casino/src/casino/Croupier.java
+++ Casino/src/casino/Croupier.java
@@ -5,8 +5,15 @@ public class Croupier extends Persons {
 	public Croupier (String name) {
 		super(name);
 	}
-	public checkPlace(int place, Player player) {
-		if((player.getCredits() + place)/2 > place)
+	public boolean checkPlace(int place, Player player) {
+		if((player.getCredits() + place)/2 > place) {
+			System.out.println("Ungültiger Einsatz du Setzt mehr wie die Hälfte deiner Credits");
+			System.out.printf("Du hast %d Credits zurück gebucht bekommen\n", place);
+			return false;
+		}else {
+			System.out.printf("Crouper %s hat den Einsatz %s Credits akzeptiert \n", getName(), place);
+			return true;
+		}
 	}
 	@Override
 	public boolean performRole() {
