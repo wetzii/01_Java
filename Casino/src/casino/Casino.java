90ba570821906c934197adc4271e38defcb68ccd
wetzi <wetzi@Samsung-Degl-ARCH>
2026-01-14 12:38:48 +0100

    Fast Fertig

--- Casino/src/casino/Casino.java
+++ Casino/src/casino/Casino.java
@@ -1,5 +1,36 @@
 package casino;
 
-public class Casino {
+import java.util.Scanner;
 
+public class Casino {
+/*Casino implementiert den kompletten Casino-Aufenthalt. Zuerst wird der Spieler
+nach seinem Namen gefragt und mit wievielen Credits er spielt (Credits müssen
+mindestens 30 sein). Anschließend werden vom Spieler und vom Croupier die per-
+formRole() ausgegeben. Die unterschiedlichn Spiele, die es im Casino gibt, werden
+in einem Array gespeichert. Der Nutzer hat die Möglichkeit aus 4 verschiedenen
+Menü-Punkten zu wählen (Slot Maschine, Roulette, Credits anzeigen, Exit). Wird die
+Slot-Maschine oder Roulette ausgewählt, so setzt der Player zuerst seinen Einsatz
+bevor die jeweiligen Aktionen ausgeführt werden. Wenn der Player einen ungültigen
+Einsatz setzt (siehe Methode in Player), dann soll er nochmal den Einsatz setzten
+können*/
+	public static void printLogo() {
+	System.out.println("  _  __                 _  ___         _      __  \n"
+			+ " | |/ /___ _ _ _ _  ___| |/ __|__ _ __(_)_ _ /  \\ \n"
+			+ " | ' </ -_) '_| ' \\/ -_) | (__/ _` (_-< | ' \\ () |\n"
+			+ " |_|\\_\\___|_| |_||_\\___|_|\\___\\__,_/__/_|_||_\\__/ \n"
+			+ "                                                  ");
+	}
+	public static void main(String[] args) {
+		printLogo();
+		Scanner scanner = new Scanner(System.in);
+	}
+	public static Player createPlayer(Scanner scan) {
+	System.out.println("Gib deinen Namen ein: ");
+	Player player = new Player(scan.nextLine());
+	return player;
+	}
+	public static Croupier createCroupier() {
+		Croupier croupier = new Croupier("Nino");
+		return croupier;
+	}
 }
---
+++ Casino/src/casino/SlotMaschine.java
@@ -0,0 +1,61 @@
+package casino;
+
+import java.util.Scanner;
+
+public class SlotMaschine  implements Playable{
+/*
+ SlotMachine prüft beim Spieldurchlauf zuerst ebenfalls, ob der Croupier den Einsatz
+akzeptiert. Wenn ja, werden zufällig zwei Zahlen zwischen 1 und 4 gedreht. Sind
+beide gleich, erhält der Spieler das 5fache seines Einsatzes und eine winMessage,
+ansonsten wird eine loseMessage angezeigt
+ */
+	
+public int[] spin() {
+	int[] slotMaschine = new int[5];
+	for (int i = 0; i >5; i++) {
+		slotMaschine[i] = ((int) Math.random()*9) +1;
+	}
+	return slotMaschine;
+}
+@Override
+public void playRound(Player player, Croupier croupier, int place) {
+	if(croupier.checkPlace(place, player)) {
+		
+		System.out.println("Willkomen bei Slot-Maschiene");
+		System.out.println("Hast du 3 Gleiche Zahlen Bekommst du das 12 Fache");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("(Belibige Taste) um fortzufahren");
+		scan.next();
+		int[] nums = spin();
+		System.out.println("-X-X-X-X-X-X-X-X-X-X-X-X-X");
+		System.out.printf("|[%d] [%d] [%d] [%d] [%d]|\n",nums[0], nums[1], nums[2], nums[3],nums[4]);
+		System.out.println("--------------------------------------");
+		boolean allTrue = true;;
+		for(int i = 0; i > nums.length; i++) {
+			if(nums[0] != nums[i]) {
+				allTrue = false;
+				}
+			}
+		if(allTrue) {
+			winMessage(player);
+			addReward(place, 50, player);
+		}else {
+		loseMessage(player);
+			}
+		scan.close();
+		}
+	}
+
+@Override
+	public void loseMessage(Player player) {
+		System.out.printf("%s hat bei Slot Maschiene Leider Verloren Versuche es nochmal! \n",player.getName());
+	}
+@Override
+	public void winMessage(Player player) {
+		System.out.printf("%s hat Bei Slot Maschine Gewonnen TOLL!!!\n", player.getName());
+	}
+@Override
+	public void addReward(int place, int rewardMultiplier, Player player) {
+		player.addCredits(rewardMultiplier* place);
+	}
+}
