cbb73591e3142311dab864875012beae433e54dd
wetzi <wetzi@Samsung-Degl-ARCH>
2026-03-02 11:36:23 +0100

    Mediathek

--- CalculatorAdv/src/calc/Logarithm.java
+++ CalculatorAdv/src/calc/Logarithm.java
@@ -4,6 +4,342 @@ public class Logarithm extends Operators {
 	
 	public Logarithm(int[] nums) {
 		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public double calc(int[] nums) {
+		check();
+		double result = Math.log(nums[0]) / Math.log(nums[1]);
+		return result;
+	}
+	@Override
+	public void check() {
+		
+		if(getNum(0)  < 0 ) {
+			throw new IllegalArgumentException("Die Basis muss positiv sein!");
+		}
+		if(getNum(0)== 1) {
+			throw new IllegalArgumentException("Die Basis soll nicht '1' sein");
+		
+		}if( getNum(1) <= 0){
+			throw new IllegalArgumentException("Der Numerus darf nicht negativ sein");
+			
+		}
+	}
+	@Override
+	public void printResult(int[] nums, double result) {
+		System.out.printf("Der Logarythmus von %d  mit der Basis %d = %f.4 \n", nums[1], nums[0],result );
+	}
+
+}
+package calc;
+
+public class Logarithm extends Operators {
+	
+	public Logarithm(int[] nums) {
+		super(nums, "Logaeythmus");
+	}private void vdfbf() {
+		// TODO Auto-generated method stub
+
 	}
 	
 	@Override
---
+++ Mediathek/src/mediathek/DigitalMedia.java
@@ -0,0 +1,33 @@
+package mediathek;
+
+public class DigitalMedia {
+	
+	private String title;
+	private String genre;
+	private int[] ratingArray;
+	private float avgRating;
+	private int countRating;
+	
+	public DigitalMedia(String title, String genre) {
+		this.title = title;
+		this.genre = genre;
+		avgRating = 0;
+		countRating = 0;
+		ratingArray = new int[5];
+	}
+	public void Rate(int rating) {
+	if(countRating >= ratingArray.length) {
+		throw new IllegalArgumentException("Kein Platz für weitere Bewertungen");
+	}
+	else if(rating < 0 || rating > 5  ) {
+		throw new IllegalArgumentException("Die Bewertung muss zwischen 0 und 5 Sterne sein");
+		}
+	}
+	public float calcAvgRating(int[] nums) {
+		int sum = 0;
+		for (int n: nums ) {
+			sum += n;
+		}
+		return sum / countRating;
+	}
+}
