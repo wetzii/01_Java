ee1db7badf32f80504ec73e4b5ecc46cd3dfa807
wetzi <wetzi@Samsung-Degl-ARCH>
2026-03-02 10:48:06 +0100

    TOP

--- Instanzen_Demo/src/instanzen/Person.java
+++ Instanzen_Demo/src/instanzen/Person.java
@@ -6,4 +6,7 @@ public class Person {
 	public Person(String name) {
 		this.name = name;
 	}
+	public String getName() {
+		return name;
+	}
 }
--- Instanzen_Demo/src/instanzen/RunCode.java
+++ Instanzen_Demo/src/instanzen/RunCode.java
@@ -3,6 +3,19 @@ package instanzen;
 public class RunCode {
 	
 	public static void main(String[] args) {
+			Person[] persons = new Person[2];
+			
+			persons[0] = new Person("Matthias Oh Nein");
+			persons[1] = new Students("Cora", 100029);
+			
+			if(persons[1] instanceof Students) {
+				//Variante a: 
+				Students s = (Students) persons[1];
+				s.getMatNr();
+				
+				//Variante b: 
+				((Students)persons[1]).getMatNr();
+			}
 			
 		}
 }
