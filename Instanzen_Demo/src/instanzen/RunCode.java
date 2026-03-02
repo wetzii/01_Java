9746fdd73a77d65e5150d59250cc6db9ef612e3a
wetzi <wetzi@Samsung-Degl-ARCH>
2026-03-02 11:02:26 +0100

    Fertig

--- Instanzen_Demo/src/instanzen/RunCode.java
+++ Instanzen_Demo/src/instanzen/RunCode.java
@@ -5,7 +5,7 @@ public class RunCode {
 	public static void main(String[] args) {
 			Person[] persons = new Person[2];
 			
-			persons[0] = new Person("Matthias Oh Nein");
+			persons[0] = new Person("Matthias --> Mätzga");
 			persons[1] = new Students("Cora", 100029);
 			
 			if(persons[1] instanceof Students) {
@@ -16,6 +16,11 @@ public class RunCode {
 				//Variante b: 
 				((Students)persons[1]).getMatNr();
 			}
-			
+			for(Person p : persons) {
+				System.out.println(p.getName());
+			}
+			for(int idx = 0; idx < persons.length; idx++) {
+				System.out.println(persons[idx].getName());
+			}
 		}
 }
